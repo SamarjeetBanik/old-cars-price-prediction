82eb2642fc8e861e0f18f4275357a27e38699e27
SamarjeetBanik <samarjeetbanik@gmail.com>
2021-10-31 20:09:48 +0530

    [add]: flask files for web page

---
+++ application.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+df = pd.read_csv("Filtered_Data.csv")
+
+@app.route("/")
+def index():
+    car_models = sorted(df["name"].unique())
+    companies = sorted(df["company"].unique())
+    purchase_year = sorted(df["year"].unique(), reverse = True)
+    fuel_type = sorted(df["fuel_type"].unique())
+
+    return render_template("index.html", car_models = car_models, companies = companies, purchase_year = purchase_year, fuel_type = fuel_type)
+
+if __name__ == "__main__":
+    app.run(debug = True)
